15dbece3c75509209a6bc9ab0a9857fdf80842a5
Matteo Palagi <mpalagi859@gmail.com>
2026-02-20 16:01:51 +0100

    update barrel explosion

--- Networking-Fishnet/Assets/Scripts/Exercises/Exc01/Barrel.cs
+++ Networking-Fishnet/Assets/Scripts/Exercises/Exc01/Barrel.cs
@@ -1,18 +1,30 @@
 using UnityEngine;
+using System.Collections;
 using FishNet.Object;
 
 public class Barrel : NetworkBehaviour
 {
+
     private FPSHurtBox _hurtBox;
     private void Awake()
     {
-        _hurtBox = GetComponentInChildren<FPSHurtBox>();
+        _hurtBox = GetComponentInChildren<FPSHurtBox>(includeInactive : true);
     }
+    [ServerRpc (RequireOwnership = false)]
     public void Explode()
     {
+        Debug.Log("Exploding barrel...");
         if (_hurtBox == null) return;
         _hurtBox.EnableCollider(true);
         Debug.Log("Barrel exploded!");
+        StartCoroutine(ResetAfterCooldown(1f));
         transform.gameObject.SetActive(false);
+    }
+    private IEnumerator ResetAfterCooldown(float cooldown)
+    {
+        Debug.Log("Resetting barrel after cooldown...");
+        yield return new WaitForSeconds(cooldown);
+        Debug.Log("Reset barrel...");
+        _hurtBox.EnableCollider(false);
     }
 }

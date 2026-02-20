d835ac57bbf900f075b5b213fd117b17e7f693a0
Matteo Palagi <mpalagi859@gmail.com>
2026-02-20 13:55:27 +0100

    added barrel explosion

--- Networking-Fishnet/Assets/Scripts/Exercises/Exc01/Barrel.cs
+++ Networking-Fishnet/Assets/Scripts/Exercises/Exc01/Barrel.cs
@@ -3,5 +3,16 @@ using FishNet.Object;
 
 public class Barrel : NetworkBehaviour
 {
-     // Add your code
+    private FPSHurtBox _hurtBox;
+    private void Awake()
+    {
+        _hurtBox = GetComponentInChildren<FPSHurtBox>();
+    }
+    public void Explode()
+    {
+        if (_hurtBox == null) return;
+        _hurtBox.EnableCollider(true);
+        Debug.Log("Barrel exploded!");
+        transform.gameObject.SetActive(false);
+    }
 }
